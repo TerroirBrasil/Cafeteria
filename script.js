e0cc397f1c8b46c8070b68851ee77aa9d6fa0346
vic-rimbano <130366712+vic-rimbano@users.noreply.github.com>
2026-06-05 00:59:22 -0300

    Add filtering feature for coffee cards
    Implement filter functionality for cards based on button clicks.

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ function saudacao() {
         "Bem-vindo à Terroir Brasil! ☕\n\nDescubra os sabores únicos dos cafés especiais brasileiros."
     );
 }
+
+const botoes = document.querySelectorAll(".filtro-btn");
+const cards = document.querySelectorAll(".card");
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        botoes.forEach(btn => btn.classList.remove("active"));
+        botao.classList.add("active");
+
+        const categoria = botao.dataset.categoria;
+
+        cards.forEach(card => {
+
+            if(
+                categoria === "todos" ||
+                card.dataset.categoria === categoria
+            ){
+                card.classList.remove("oculto");
+            } else {
+                card.classList.add("oculto");
+            }
+
+        });
+
+    });
+
+});
